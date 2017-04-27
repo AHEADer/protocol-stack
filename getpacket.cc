e8ee2adf3730e2b4eaa8a41f4819fd26578f0c8a
AHEADer <dqawveird@gmail.com>
2017-04-27 12:18:50 +0800

    interrupt

--- getpacket.cc
+++ getpacket.cc
@@ -7,10 +7,15 @@
 #include <net/ethernet.h> /* the L2 protocols */
 #include "utils.h"
 
+#define SIZE 1024
 char *get_interface_name();
 
 int main(int argc, char *argv[])
 {
+	if (argc==1)
+	{
+		fprintf(stderr, "need argument\n");
+	}
 	int fd;
 	fd=socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	if (fd < 0)
@@ -27,6 +32,27 @@ int main(int argc, char *argv[])
         fprintf(stderr ,"bind error\n");
         exit(-1);
     }
-    //connect()
+    char buffer[SIZE];
+    //printf("%s\n", argv[1]);
+    if (argv[1][0]=='1')
+    {
+    	/*if (sendto(fd, buffer, SIZE, 0, (struct sockaddr *) &eth_addr ,sizeof(struct sockaddr_ll)) == -1 )
+    	{
+        	fprintf(stderr ,"sendto error\n");
+        	return -1;
+    	}*/
+
+    	connect(fd, (struct sockaddr *)&eth_addr, sizeof(struct sockaddr_ll));
+    	strcpy(buffer, "happy");
+    	write(fd, buffer, SIZE);
+    }
+    else
+    {
+    	fprintf(stderr, "listening:\n");
+    		/*if(recvfrom(fd, buffer, SIZE, 0, (struct sockaddr *) &eth_addr, )>0)
+    			printf("buff is %s\n", buffer);*/
+    	//listen(fd, )
+    }
+    
 	return(0);
 }
